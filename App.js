f40d5c2a1c4094abd57d594e922963da173f681a
Rudransh-K <rudranshkulkarniofficial@gmail.com>
2022-02-04 00:30:25 +0530

    Tasks View
    All the tasks are now seen. Need to add functionality of adding new tasks and deleting them.

--- App.js
+++ App.js
@@ -1,11 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/singleTask';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.taskWrapper}>
+        <Text style={styles.listTitle}>
+          Today's Tasks
+        </Text>
+        <View style={styles.taskItems}>
+          <Task text="Zeroth Task" />
+          <Task text="First Task" />
+        </View>
+      </View>
     </View>
   );
 }
@@ -13,8 +21,23 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#219F94',
+
   },
+  taskWrapper: {
+    paddingTop: 40,
+    paddingLeft: 22,
+  },
+  listTitle: {
+    fontSize: 30,
+    fontWeight: 'bold',
+    color: '#F2F5C8',
+    paddingTop: 10
+  },
+  taskItems: {
+    paddingTop: 30,
+    paddingLeft: 5,
+    paddingRight: 27
+  },
+
 });
